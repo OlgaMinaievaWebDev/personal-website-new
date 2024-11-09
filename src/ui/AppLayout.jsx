1cc5a7782bd074730a9248f1d0ea8d2223183e8a
Olga <minaeva9@gmail.com>
2024-11-09 15:23:22 -0500

    header adjusted responsive and icon imported

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -6,7 +6,7 @@ function AppLayout() {
   return (
     <div className="min-h-screen flex flex-col bg-background">
       <Header />
-      <main className="flex-grow p-5">
+      <main className="flex-grow p-5 sm:px-8 md:px-10 lg:px-12 xl:px-16">
         <Outlet />
       </main>
       <Footer />
--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,16 +1,17 @@
 import { NavLink } from "react-router-dom";
 import Logo from "/public/Olga-Logo.webp";
+import { FaBars } from "react-icons/fa";
 
 function Header() {
   return (
     <header className="flex justify-between items-center p-5 bg-background text-textPrimary">
       <h1 className="text-2xl font-bold mb-2 hover:text-highlight">
         <NavLink to="/">
-          <img src={Logo} alt="" className="h-16 mb-2" />
+          <img src={Logo} alt="Logo" className="h-16 mb-2" />
         </NavLink>
       </h1>
 
-      <nav className="flex space-x-6">
+      <nav className="hidden sm:flex space-x-6">
         <NavLink
           to="/"
           className={({ isActive }) =>
@@ -52,6 +53,13 @@ function Header() {
           Contact
         </NavLink>
       </nav>
+
+      {/* Mobile Menu Toggle */}
+      <div className="sm:hidden">
+        <button className="text-textPrimary">
+          <FaBars />
+        </button>
+      </div>
     </header>
   );
 }
