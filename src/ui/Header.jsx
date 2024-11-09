7cec07eb44f3e972ce990ba4956948c789bd1993
Olga <minaeva9@gmail.com>
2024-11-09 14:49:04 -0500

    add isActive class to NavLink in Header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -7,11 +7,46 @@ function Header() {
         <NavLink to="/">Olga</NavLink>
       </h1>
       <nav className="flex space-x-4">
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/projects">Projects</NavLink>
-        <NavLink to="/skills">Skills</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
+        <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive ? "text-orange-500 underline" : "hover:underline"
+          }
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) =>
+            isActive ? "text-orange-500 underline" : "hover:underline"
+          }
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/projects"
+          className={({ isActive }) =>
+            isActive ? "text-orange-500 underline" : "hover:underline"
+          }
+        >
+          Projects
+        </NavLink>
+        <NavLink
+          to="/skills"
+          className={({ isActive }) =>
+            isActive ? "text-orange-500 underline" : "hover:underline"
+          }
+        >
+          Skills
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={({ isActive }) =>
+            isActive ? "text-orange-500 underline" : "hover:underline"
+          }
+        >
+          Contact
+        </NavLink>
       </nav>
     </header>
   );
